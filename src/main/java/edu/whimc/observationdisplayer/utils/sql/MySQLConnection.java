015bf463566f1a24cf25295aa027e88d33cf2eda
Jack Henhapl <jackah2@illinois.edu>
2021-03-18 03:37:06 -0500

    Removing old query

--- src/main/java/edu/whimc/observationdisplayer/utils/sql/MySQLConnection.java
+++ src/main/java/edu/whimc/observationdisplayer/utils/sql/MySQLConnection.java
@@ -11,24 +11,6 @@ public class MySQLConnection  {
 
     public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
     public static final String URL_TEMPLATE = "jdbc:mysql://%s:%s/%s";
-    public static final String CREATE_TABLE =
-            "CREATE TABLE IF NOT EXISTS `whimc_observations` (" +
-            "  `rowid`       INT    AUTO_INCREMENT NOT NULL," +
-            "  `time`        BIGINT                NOT NULL," +
-            "  `uuid`        VARCHAR(36)           NOT NULL," +
-            "  `username`    VARCHAR(16)           NOT NULL," +
-            "  `world`       VARCHAR(64)           NOT NULL," +
-            "  `x`           DOUBLE                NOT NULL," +
-            "  `y`           DOUBLE                NOT NULL," +
-            "  `z`           DOUBLE                NOT NULL," +
-            "  `yaw`         FLOAT                 NOT NULL," +
-            "  `pitch`       FLOAT                 NOT NULL," +
-            "  `observation` TEXT                  NOT NULL," +
-            "  `active`      BOOLEAN               NOT NULL," +
-            "  `expiration`  BIGINT                        ," +
-            "  PRIMARY KEY    (`rowid`)," +
-            "  INDEX uuid     (`uuid`)," +
-            "  INDEX username (`username`));";
 
     private Connection connection;
     private String host, database, username, password, url;
