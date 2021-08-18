aa51889a3d4328b696c9430afc97a33260cd25a8
Jack Henhapl <jackah2@illinois.edu>
2021-08-18 13:21:28 -0500

    Removing unnecessary method

--- src/main/java/edu/whimc/observations/observetemplate/models/ObservationType.java
+++ src/main/java/edu/whimc/observations/observetemplate/models/ObservationType.java
@@ -1,11 +1,6 @@
 package edu.whimc.observations.observetemplate.models;
 
-import edu.whimc.observations.Observations;
 import org.apache.commons.lang.StringUtils;
-
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public enum ObservationType {
 
@@ -23,12 +18,6 @@ public enum ObservationType {
     @Override
     public String toString() {
         return StringUtils.capitalize(super.toString().toLowerCase());
-    }
-
-    public Set<ObservationType> getConfiguredTypes(Observations plugin) {
-        return Arrays.stream(this.values())
-                .filter(type -> plugin.getConfig().isSet("templates." + type))
-                .collect(Collectors.toSet());
     }
 
 }
