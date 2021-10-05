394103a0146f6c941c41e56b06a6d424553cdeeb
Sam Hum <s_hum@coloradocollege.edu>
2021-10-05 08:18:48 -0500

    Adds uncolored versions to existing observations

--- src/main/java/edu/whimc/observations/utils/sql/migration/schemas/Schema_4.java
+++ src/main/java/edu/whimc/observations/utils/sql/migration/schemas/Schema_4.java
@@ -1,6 +1,8 @@
 package edu.whimc.observations.utils.sql.migration.schemas;
 
+import edu.whimc.observations.utils.Utils;
 import edu.whimc.observations.utils.sql.migration.SchemaVersion;
+import org.bukkit.ChatColor;
 
 import java.sql.*;
 
@@ -8,6 +10,21 @@ public class Schema_4 extends SchemaVersion {
 
     private static final String ADD_CATEGORY =
             "ALTER TABLE whimc_observations ADD COLUMN observation_color_stripped TEXT;";
+
+    /**
+     * Query to update existing observations with an uncolored version
+     */
+    private static final String QUERY_UPDATE_OBSERVATION_NO_COLOR =
+            "UPDATE whimc_observations " +
+                    "SET observation_color_stripped=?" +
+                    "WHERE rowid=?";
+    /**
+     * Query for getting all observations from the database.
+     */
+    private static final String QUERY_GET_OBSERVATIONS =
+            "SELECT * " +
+                    "FROM whimc_observations " +
+                    "WHERE (observation_color_stripped is null or observation_color_stripped = '')";
 
     public Schema_4() {
         super(4, null);
@@ -17,6 +34,18 @@ public class Schema_4 extends SchemaVersion {
     protected void migrateRoutine(Connection connection) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement(ADD_CATEGORY)) {
             statement.execute();
+        }
+
+        try (PreparedStatement statement = connection.prepareStatement(QUERY_UPDATE_OBSERVATION_NO_COLOR)) {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY_GET_OBSERVATIONS);
+            while (rs.next()) {
+                String observation = rs.getString("observation");
+                int rowid = rs.getInt("rowid");
+                statement.setString(1, ChatColor.stripColor(Utils.color(observation)));
+                statement.setInt(2, rowid);
+                statement.execute();
+            }
         }
     }
 
